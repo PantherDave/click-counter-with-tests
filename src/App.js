cec1a4e28b9313f5301b96adcf522009a4a7c673
PantherDave <dacos062@fiu.edu>
2021-11-21 20:18:06 -0500

    Tests the count and increment functionality

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [ctr, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
   return (
     <div data-test="component-app">
-      <h1 data-test="counter-display">The count is {ctr}</h1>
-      <button data-test="increment-button" onClick={() => setCount(ctr + 1)}>
+      <h1 data-test="counter-display">
+        The count is&nbsp; <span data-test="count">{count}</span>
+      </h1>
+      <button data-test="increment-button" onClick={() => setCount(count + 1)}>
         Increment counter
       </button>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -33,6 +33,22 @@ test("renders counter display", () => {
   expect(counterDisplay.length).toBe(1);
 });
 
-test("counter display starts at 0", () => {});
+test("counter display starts at 0", () => {
+  const wrapper = setup();
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("0");
+});
 
-test("clicking button increments counter display", () => {});
+test("clicking button increments counter display", () => {
+  const wrapper = setup();
+
+  // find the button
+  const button = findByTestAttr(wrapper, "increment-button");
+
+  // click the button
+  button.simulate("click");
+
+  // find the display, and test that the number has been incremented
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("1");
+});
